5befda5a7d0db72e3e98272a82ddd8a8923519c6
CARLITOS <rodrigo@idat.pe>
2022-06-13 14:50:51 -0500

    mini cambio

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ config();
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/crud-mongo";
 
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 5000;
